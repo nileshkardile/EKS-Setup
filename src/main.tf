c131884a3b6d9b8c2f88bb4ee89e0aa9e60a3f13
nileshkardile <nileshkardile831@gmail.com>
2021-01-05 07:51:59 +0000

    added backend s3 bucket for tf state

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.11.8"
+
+  backend "s3" {
+    bucket         = "devsecops-hackathon-eks-tf-state"
+    key            = "terraform.tfstate"
+    region         = "us-west-1"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region  = "us-west-1"
   version 			= ">= 3.3.0"
